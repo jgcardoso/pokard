7db622a034dd489438e7497af3bf7dc03dd7a10c
Jonas Guedes <jonaskof@gmail.com>
2021-09-25 03:09:16 -0300

    html header

--- components/Layout.js
+++ components/Layout.js
@@ -6,13 +6,6 @@ import pokemon from 'pokemontcgsdk'
 pokemon.configure({apiKey: 'd0ba1780-c71e-4d51-b328-e0c6b97891ea'})
 
 export default function Layout({ children }) {
-
-  pokemon.card.where({ pageSize: 10, page: 1 })
-  .then(result => {
-      console.log(result.data)
-  })
-
-
   return (
     <>
       <Header />
