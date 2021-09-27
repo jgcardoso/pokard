eb1112973287ae4bcd8d7243a13b47d6cc1034f9
Jonas Guedes <jonaskof@gmail.com>
2021-09-27 17:31:21 -0300

    Pagina de procura

--- components/Header.js
+++ components/Header.js
@@ -1,25 +1,45 @@
 import React from 'react'
-import styles from './../styles/Header.module.css';
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+
+import styles from './../styles/Header.module.css'
 
 function Header() {
+  const router = useRouter()
+
+  const [inputSearch, setInputSearch] = React.useState('')
+
+  function handleForm(e) {
+    e.preventDefault()
+
+    if(!inputSearch.trim()) return
+
+    router.push(`/search/${inputSearch}`)
+  }
+
   return (
     <header className={styles.header}>
       <div className="container">
         <div className={styles.headerGrid}>
         
           <div className={styles.headerLogo}>
-            <a href="#">
-              <span className={styles.icoLogo}></span>
-              Pokard
-            </a>
+            <Link href="/">
+              <a>
+                <span className={styles.icoLogo}></span>
+                Pokard
+              </a>
+            </Link>
           </div>
           <div className={styles.headerMenu}>
             <nav>
               <a href="#">Cards</a>
               <a href="#">Sets</a>
-              <a href="#">Types</a>
-              <a href="#">Rarities</a>
             </nav>
+            <div className={styles.formSearch}>
+              <form onSubmit={handleForm}>
+                <input type="text" placeholder="Search" onChange={(event) => setInputSearch(event.target.value)} />
+              </form>
+            </div>
           </div>
 
         </div> 
---
+++ pages/search/[search].js
@@ -0,0 +1,83 @@
+import React from 'react'
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+import Image from 'next/image'
+
+import styles from '../../styles/Home.module.css'
+import cardStyles from '../../styles/Cards.module.css'
+
+import pokemon from 'pokemontcgsdk'
+
+function search() {
+  const [cards, setCards] = React.useState([])
+  const [notFound, setNotFound] = React.useState(false);
+
+  const router = useRouter()
+  const { search } = router.query
+
+  React.useEffect(() => {
+    getCards();
+  }, [search])
+
+  async function getCards() {
+    setNotFound(false)
+    const response = await pokemon.card.where({ q: `name:${search}*` })
+    
+    if(response.data.length === 0) {
+      setNotFound(true);
+      setCards([])
+      return;
+    }
+    
+    setNotFound('');
+    setCards(response.data)
+  }
+
+  if(notFound) {
+    return (
+      <div className="page">
+        <div className="container">
+          <h1>Search</h1>
+          <p>Nada Encontrado</p>
+        </div>
+      </div>
+    )
+  };
+
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Search: {search} - Pokard</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className="page">
+        <div className="container">
+          
+          <h1>Search: {search}</h1>
+
+          <div className={cardStyles.grid}>
+
+            {cards && cards.map((card, index) => {
+              return (
+                <div key={card.id + index} className={cardStyles.card}>
+                  <div className={cardStyles.image}>
+                    <Image src={card.images.small ? card.images.small : card.images.large} width="245" height="342" />
+                  </div>
+                  <div className={cardStyles.name}>
+                    <p>{card.name}</p>
+                  </div>
+                </div>
+              )
+            })}
+          </div>
+
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default search
